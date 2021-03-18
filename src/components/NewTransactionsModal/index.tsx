72328d3f05d034defdbf8060ef104c3ac22292b3
joelmss93 <joelmss93@gmail.com>
2021-03-17 22:48:55 -0300

    style: modal global styles from application

--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -8,11 +8,29 @@ interface NewTransactionModalProps {
 
 export function NewTransactionsModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
   return (
-    <Modal 
-        isOpen={isOpen}
-        onRequestClose={onRequestClose}
-      >
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <Container>
         <h2>Cadastrar transação</h2>
-      </Modal>
+
+        <input
+          type="text"
+          placeholder="Título"
+        />
+        <input
+          type="number"
+          placeholder="Valor"
+        />
+        <input
+          type="text"
+          placeholder="Categoria"
+        />
+        <button type="submit">Cadastrar</button>
+      </Container>
+    </Modal>
   );
 }
